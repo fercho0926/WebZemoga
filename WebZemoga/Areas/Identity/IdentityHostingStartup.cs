dd868fb7432fc21ab903c88bce9fd6f9833b2fe6
Milton ferney Vasquez Herrera <milton.vasquez0726@gmail.com>
2021-05-28 19:36:56 -0500

    net.core

---
+++ WebZemoga/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using WebZemoga.Data;
+
+[assembly: HostingStartup(typeof(WebZemoga.Areas.Identity.IdentityHostingStartup))]
+namespace WebZemoga.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<WebZemogaContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("WebZemogaContextConnection")));
+
+                services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                    .AddEntityFrameworkStores<WebZemogaContext>();
+            });
+        }
+    }
+}
