ffa22c2ea88fd39e273bc6fdf1d5c52d1e0d8bc5
Milton ferney Vasquez Herrera <milton.vasquez0726@gmail.com>
2021-05-28 19:41:23 -0500

    WEBFORMS

---
+++ Services/AccessService.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ComTrans;
+
+namespace Services
+{
+    public class AccessService
+    {
+
+
+        public bool SaveForm(BlogClass blog)
+        {
+            try
+            {
+            //    blog blogInsert = new blog
+            //    {
+
+            //        authorName = form.authorName,
+            //        tittle = form.tittle,
+            //        content = form.content,
+
+            //        date = form.date,
+            //        status = form.status
+            //    };
+
+            //    bd.blog.Add(blogInsert);
+            //    bd.SaveChanges();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+        }
+
+
+    }
+}
---
+++ zemoga_web/Blog/Blog.aspx.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using ComTrans;
+using Microsoft.AspNet.Identity;
+using Facade;
+
+namespace zemoga_web.Blog
+{
+    public partial class Blog : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        Fcd facade = new Fcd();
+
+
+        protected void Save_Click(object sender, EventArgs e)
+        {
+
+            BlogClass blog = new BlogClass();
+            blog.authorName = User.Identity.GetUserName();
+            blog.tittle = txt_tittle.Text;
+            blog.content = txt_content.Text;
+            blog.date = DateTime.Now;
+            blog.status = "P"; // Pending Status
+
+            if (facade.SaveForm(blog))
+            {
+                ScriptManager.RegisterClientScriptBlock(this, this.GetType(), "alertMessage", "alert(' Inserted Successfully')", true);
+            }
+            else
+            {
+                ScriptManager.RegisterClientScriptBlock(this, this.GetType(), "alertMessage", "alert(' Inser Record Failed ')", true);
+            }
+            Response.Redirect("~/Default.aspx");
+        }
+    }
+}
